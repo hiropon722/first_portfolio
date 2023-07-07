ed978a320805931c18985112b77e91d92b816369
hiropon722 <liera722@gmail.com>
2023-07-07 13:16:20 +0900

    tasklist install

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+    def check_process_status(process_names)
+        running_processes = []
+    
+        process_names.each do |process_name|
+          result = `tasklist /FI "IMAGENAME eq #{process_name}" /NH`
+          running_processes << process_name unless result.empty?
+        end
+    
+        if running_processes.empty?
+          "指定されたプロセスはいずれも実行されていません"
+        else
+          "以下のプロセスが実行中です: #{running_processes.join(', ')}"
+        end
+    end
 end
